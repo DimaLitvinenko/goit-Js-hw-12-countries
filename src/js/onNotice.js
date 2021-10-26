cbc2d347ef8068ee1452e68cc78a674a4ba5a85c
DimaLitvinenko <80059894+DimaLitvinenko@users.noreply.github.com>
2021-10-26 15:20:58 +0300

    update

--- src/js/onNotice.js
+++ src/js/onNotice.js
@@ -1,9 +1,10 @@
 import '@pnotify/core/dist/PNotify.css'
-import '@pnotify/core/dist/BrightTheme.css'
+import '@pnotify/core/dist/Angeler.css';
 import 'material-design-icons/iconfont/material-icons.css'
+import  '@pnotify/mobile/dist/PNotifyMobile.css'; 
 
-import { error, defaultModules } from '@pnotify/core'
-import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js'
+import { error, defaultModules, Stack } from '@pnotify/core'
+import * as PNotifyMobile from '@pnotify/mobile';
 
 defaultModules.set(PNotifyMobile, {});
 
@@ -14,10 +15,20 @@ export default function showError(errorMessage) {
     delay: 800,
     sticker: true,
     mode: 'light',
+    shadow: true,
     width: '100%',
+    mouseReset: true,
     maxTextHeight: null,
-    icons: 'brighttheme',
+    styling: 'angeler',
+    icons: 'angeler',
+    addClass : 'angeler-extended', 
     closer: true,
     closerHover: false,
+    stack : new Stack ({ 
+      dir1: 'down',  
+      dir2: 'right',  // Позиция от верхнего левого угла. 
+      firstpos1: 90,  
+      firstpos2: 90  // 90 пикселей сверху, 90 пикселей слева. 
+    }) 
   });
 }
